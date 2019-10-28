d852e72271ceb8040327781ca2ce889da01636c6
Erik Wegner <E_Wegner@web.de>
2019-10-28 19:26:44 +0100

    First models

--- wamytmsite/wamytmapp/admin.py
+++ wamytmsite/wamytmapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import OrgUnit, ProductTeam
+
+admin.site.register(OrgUnit)
+admin.site.register(ProductTeam)
--- wamytmsite/wamytmapp/models.py
+++ wamytmsite/wamytmapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class OrgUnit(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductTeam(models.Model):
+    orgunit = models.ForeignKey(OrgUnit, on_delete=models.PROTECT)
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
