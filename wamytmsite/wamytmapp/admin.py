69a27b956827ea4b4f74c686a6b5167d4a43f2e8
Erik Wegner <E_Wegner@web.de>
2019-10-28 19:56:30 +0100

    Team member, merge org unit and product team

--- wamytmsite/wamytmapp/admin.py
+++ wamytmsite/wamytmapp/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
 
-from .models import OrgUnit, ProductTeam
+from .models import OrgUnit, TeamMember
 
 admin.site.register(OrgUnit)
-admin.site.register(ProductTeam)
+
+# Define an inline admin descriptor for TeamMember model
+# which acts a bit like a singleton
+class TeamMemberInline(admin.StackedInline):
+    model = TeamMember
+    can_delete = False
+    verbose_name_plural = 'team members'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (TeamMemberInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- wamytmsite/wamytmapp/models.py
+++ wamytmsite/wamytmapp/models.py
@@ -1,18 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
 
 class OrgUnit(models.Model):
     name = models.CharField(max_length=80)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
-class ProductTeam(models.Model):
+class TeamMember(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     orgunit = models.ForeignKey(OrgUnit, on_delete=models.PROTECT)
-    name = models.CharField(max_length=120)
 
-    def __str__(self):
-        return self.name
+
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
+@receiver(post_save, sender=User)
+def create_user_teammember(sender, instance, created, **kwargs):
+    if created:
+        TeamMember.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_teammember(sender, instance, **kwargs):
+    instance.teammember.save()
