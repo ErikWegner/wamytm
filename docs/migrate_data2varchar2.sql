83d68cb5840e0c154c4150ab06761bdb4cc369ba
F9616 <florian.heidenreich@regiocom.com>
2025-08-01 11:36:44 +0200

    asd

---
+++ docs/migrate_data2varchar2.sql
@@ -0,0 +1,21 @@
+declare
+  l_count integer;
+begin
+  select count(*)
+    into l_count
+    from WAMYTMAPP_TIMERANGE
+   where dbms_lob.getlength(data) > 4000;
+  if l_count > 0 then
+    raise_application_error(-20001, 'Abbruch: Es existieren Daten mit mehr als 4000 Zeichen in WAMYTMAPP_TIMERANGE.data');
+  end if;
+
+  alter table WAMYTMAPP_TIMERANGE rename column data to DATA_OLD;
+  alter table WAMYTMAPP_TIMERANGE add data varchar2(4000);
+  update WAMYTMAPP_TIMERANGE
+     set data = dbms_lob.substr(lob_loc => data_old,
+                                amount  => 4000,
+                                offset  => 0);
+  commit;
+  alter table WAMYTMAPP_TIMERANGE modify data not null;
+  alter table WAMYTMAPP_TIMERANGE drop column data_old;
+end;
